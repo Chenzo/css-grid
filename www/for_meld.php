d3713a85588e967e2cf672cd58568be23769f18a
Vince Suriani <chenzo@x3g2-5cg7140nl3.tb.tollbrothersinc.com>
2018-01-25 11:22:54 -0500

    Done meld

---
+++ www/for_meld.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>CSS Grid Examples/Tests Page 3</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
+
+        <link rel="stylesheet" href="/css/styles.css?cb=1234" />
+
+    </head>
+    <body class="meld">
+
+        <h1>CSS GRID REAL LAYOUT</h1>
+
+        <section>
+
+            <div class="mgrid">
+                <div class="one">1</div>
+                <div class="two">2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+            </div>
+
+        </section>
+
+
+    </body>
+</html>
