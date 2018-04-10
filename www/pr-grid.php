0f869e9cc6edd3a55d6e980614ccf0243a24f74a
Vince Suriani <chenzorama@hotmail.com>
2018-04-10 10:18:35 -0400

    real world test

---
+++ www/pr-grid.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>CSS Grid Examples/Tests</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
+
+        <link rel="stylesheet" href="/css/styles.css?cb=1234" />
+
+    </head>
+    <body class="pr_grid">
+
+
+        <section>
+            <div class="prGrid">
+                <div class="tl_corner">pic</div>
+                <div class="tr_text">text</div>   
+                <div class="bl_corner">pic2</div>
+                <div class="mdl_pic">pic3</div>
+                <div class="br_video">video</div>
+            </div>
+
+        </section>
+
+
+    </body>
+</html>
