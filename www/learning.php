70a055db8423fd843503774e42ae9c0b51eaaf3c
Vince Suriani <chenzorama@hotmail.com>
2022-05-02 13:24:26 -0400

    more

---
+++ www/learning.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>CSS Grid Examples/Tests</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
+
+        <link rel="stylesheet" href="/css/styles.css?cb=1234" />
+
+    </head>
+    <body>
+
+        <h1>GRID REVIEW</h1>
+
+        <div class="flexy">
+
+            <div class="one">
+                I'm Div one
+            </div>
+
+            <div class="two">
+                I'm Div two
+            </div>
+
+            <div class="two">
+                I'm Div three
+            </div>
+            
+            <div class="two">
+                I'm Div four
+            </div>
+
+        </div>
+
+        <script defer="defer" type="application/javascript" src="/js/scripts.js?cb=14567"></script>
+            
+    </body>
+</html>
